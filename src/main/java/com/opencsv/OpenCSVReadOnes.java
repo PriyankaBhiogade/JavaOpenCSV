f606ba5f68aa9ef920260bb40bd37917dc555388
PriyankaBhiogade <bhiogadepriyanka93@gmail.com>
2019-11-29 10:17:41 +0530

    Add:class to read CSV file at once

---
+++ src/main/java/com/opencsv/OpenCSVReadOnes.java
@@ -0,0 +1,30 @@
+package com.opencsv;
+
+import com.opencsv.CSVReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class OpenCSVReadOnes {
+    private static final String SAMPLE_CSV_FILE_PATH = "./users-with-header.csv";
+
+    public static void main(String[] args) throws IOException {
+        try (
+                Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
+                CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(1).build();
+
+        ) {
+            List<String[]> records = csvReader.readAll();
+            for (String[] record : records) {
+                System.out.println("Name : " + record[0]);
+                System.out.println("Email : " + record[1]);
+                System.out.println("Phone : " + record[2]);
+                System.out.println("Country : " + record[3]);
+                System.out.println("---------------------------");
+            }
+
+        }
+    }
+}
